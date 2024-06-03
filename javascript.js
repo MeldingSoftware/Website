223d053333333fb3244ec425c370e7c1985e0628
MeldingSoftware <155779315+MeldingSoftware@users.noreply.github.com>
2024-06-02 23:25:05 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -44,8 +44,26 @@ window.addEventListener('resize', function() {
     }
 });
 
+// CLOSE OVERYLAY WHEN EXCEEDING MOBILE SIZE
+window.addEventListener('resize', function() {
+    var overlay = document.getElementById('menuOverlay');
+    var hamburger = document.querySelector('nav .hamburger');
+    var closeButton = document.querySelector('.close-button');
 
-
+    // Assuming 1350px as the breakpoint for mobile to desktop transition
+    if (window.innerWidth > 1350) {
+        // Close the overlay and hide both the hamburger and close button when transitioning to desktop view
+        if (overlay.classList.contains('active')) {
+            overlay.style.display = 'none';
+            overlay.classList.remove('active');
+            closeButton.style.display = 'none';
+        }
+        hamburger.style.display = 'none';  // Ensure hamburger is hidden in desktop view
+    } else {
+        // Make sure the hamburger is visible when in mobile view
+        hamburger.style.display = 'block';
+    }
+});
 
 
 // PAYPAL
