8f834a3b594fdfafdd002f8fc13396d327df440e
MeldingSoftware <155779315+MeldingSoftware@users.noreply.github.com>
2025-01-04 15:00:29 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -44,18 +44,6 @@ window.addEventListener('resize', function() {
     }
 });
 
-// PAYPAL
-
-PayPal.Donation.Button({
-env:'production',
-hosted_button_id:'TM2PR4PEDX63N',
-image: {
-src:'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
-alt:'Donate with PayPal button',
-title:'PayPal - The safer, easier way to pay online!',
-}
-}).render('#donate-button');
-
 
 // PULL TO REFRESH
 let startY;
